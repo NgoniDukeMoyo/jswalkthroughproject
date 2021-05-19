efde775ac20224341b17009fc9fa69b7de778632
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-05-19 03:44:07 +0000

    js busy

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons=document.getElementsByTagName("button");
+
+    for (let button of buttons){
+
+    }
+
+})
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
